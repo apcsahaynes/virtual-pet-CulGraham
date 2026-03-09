3cec931457e7d2be7796bd0aca175631d8dea7df
CulGraham <cullen.graham@jpschools.org>
2026-03-09 09:22:54 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,19 @@
 public class Game {
+  
+  private String name;
+  private int energyDecrease;
+  private int happinessIncrease;
+  private int weightDecrease;
     
+  Game(String name, int energyDecrease, int happinessIncrease, int weightDecrease){
+    this.name = name;
+    this.energyDecrease = energyDecrease;
+    this.happinessIncrease = happinessIncrease;
+    this.weightDecrease = weightDecrease;
+  }
+  
+  public String getName(){return name;}
+  public int getEnergy(){return energyDecrease;}
+  public int getHappiness(){return happinessIncrease;}
+  public int getWeight(){return weightDecrease;}
 }
