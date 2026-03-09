70c2dbefedb05bbbdc30bd5e6a87d0782abbc5bb
CulGraham <cullen.graham@jpschools.org>
2026-03-09 09:23:41 -0600

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,19 @@
 public class Food {
+
+  private String name;
+  private int energyIncrease;
+  private int happinessIncrease;
+  private int weightGain;
+
+  public Food(String name, int energyIncrease, int happinessIncrease, int weightGain){
+    this.name = name;
+    this.energyIncrease = energyIncrease;
+    this.happinessIncrease = happinessIncrease;
+    this.weightGain = weightGain;
+  }
     
+  public String getName(){return name;}
+  public int getEnergy(){return energyIncrease;}
+  public int getHappiness(){return happinessIncrease;}
+  public int getWeight(){return weightGain;}
 }
